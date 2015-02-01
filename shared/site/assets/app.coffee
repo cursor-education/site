f2703538848593b6cb1e37375195b3f52f9f58f3
itspoma <itspoma@gmail.com>
2015-02-01 20:57:44 +0200

    added floating header, refactoring

--- shared/site/assets/app.coffee
+++ shared/site/assets/app.coffee
@@ -1,79 +1,152 @@
-scrollTo = (val, duration) ->
-    return if duration < 0
+#
+helpers =
+    scrollTo: (val, duration, done) ->
+        return if duration < 0
+        return if window.scrollToWorking == true
 
-    e = document.documentElement
+        e = document.documentElement
 
-    if e.scrollTop == 0
-        t = e.scrollTop
+        if e.scrollTop == 0
+            t = e.scrollTop
 
-        ++e.scrollTop
+            ++e.scrollTop
 
-        if t+1 != e.scrollTop--
-            e = document.body
+            if t+1 != e.scrollTop--
+                e = document.body
 
-    from = e.scrollTop
-    to = val
+        from = e.scrollTop
+        to = val
 
-    from = from.offsetTop if typeof from == "object"
-    to = to.offsetTop if typeof to == "object"
+        from = from.offsetTop if typeof from == "object"
+        to = to.offsetTop if typeof to == "object"
 
-    easeOutCuaic = (t) ->
-        t--
-        t*t*t+1
+        easeOutCuaic = (t) ->
+            t--
+            t*t*t+1
 
-    scrollToX = (element, x1, x2, t, v, step, operacion) ->
-        return if t < 0 || t > 1 || v <= 0
+        scrollToX = (element, x1, x2, t, v, step, operacion, done1) ->
+            return done1() if t < 0 || t > 1 || v <= 0
 
-        element.scrollTop = x1 - (x1-x2) * operacion(t);
-        t += v * step
+            element.scrollTop = x1 - (x1-x2) * operacion(t);
+            t += v * step
 
-        setTimeout(->
-            scrollToX element, x1, x2, t, v, step, operacion
-        , step)
+            setTimeout(->
+                scrollToX element, x1, x2, t, v, step, operacion, done1
+            , step)
 
-    scrollToX e, from, to, 0, 1/duration, 20, easeOutCuaic
+        window.scrollToWorking = true
 
-for el in document.getElementsByClassName('go-to-top')
-    do (el) ->
-        el.onclick = (e) ->
-            e.preventDefault();
+        scrollToX e, from, to, 0, 1/duration, 20, easeOutCuaic, ->
+            window.scrollToWorking = false
+            done()
 
-            elementID = el.getAttribute('data-top');
+    scrollHook: ->
+        helpers.scrollHookHeader()
+        helpers.scrollHookMenu()
 
-            if elementID
-                topValue = document.getElementById(elementID).offsetTop
-            else
-                topValue = 0
+    scrollHookHeader: ->
+        scroll = document.body.scrollTop
 
-            if el.hasAttribute('data-top-offset')
-                topOffset = parseInt el.getAttribute('data-top-offset')
-                topValue += topOffset
+        headerMenuEl = document.getElementById('header-menu')
+        offsetTop = document.getElementById('learn-more-button').offsetTop
 
-            scrollTo topValue, 1000
+        className = 'open'
+        classNameAnimate = headerMenuEl.getAttribute('data-animate-effect')
 
-# initParallaxEffect = ->
-#     scroll = document.body.scrollTop
+        if scroll >= offsetTop
+            headerMenuEl.classList.add(className)
+            headerMenuEl.classList.add(classNameAnimate)
+        else
+            headerMenuEl.classList.remove(className)
+            headerMenuEl.classList.remove(classNameAnimate)
 
-#     els = [
-#         document.getElementById('header')
-#         document.getElementById('section-bg-inline')
-#     ]
+    scrollHookMenuReset: ->
+        classSelected = 'selected'
 
-#     offsetY = scroll;
-#     offsetY = 0 if offsetY < 0;
+        selectedEls = document.getElementById('menu').getElementsByClassName(classSelected)
 
-#     for el in els
-#         continue unless el
+        for selectedEl in selectedEls
+            selectedEl.classList.remove(classSelected)
 
-#         isElVisible = scroll > -1
-#         isElVisible = isElVisible && scroll >= (el.offsetTop - el.offsetHeight)
-#         isElVisible = isElVisible && scroll <= (el.offsetTop + el.offsetHeight)
+    scrollHookMenu: ->
+        scroll = document.body.scrollTop
 
-#         if isElVisible
-#             bgOffset = offsetY - el.offsetTop
-#             el.style.backgroundPositionY = "#{bgOffset}px"
+        helpers.scrollHookMenuReset()
 
-# window.onscroll = ->
-#     initParallaxEffect()
+        classSelected = 'selected'
 
-# window.onscroll()
+        menuEls = document.getElementById('menu').getElementsByTagName('a')
+        offsetTopMin = -100
+
+        offsetMin = null
+        selectMenuEl = null
+
+        for menuEl in menuEls
+            dataTopEl = menuEl.getAttribute('data-top')
+
+            if dataTopEl
+                topEl = document.getElementById(dataTopEl)
+                topElOffset = scroll - (topEl.offsetTop + offsetTopMin)
+
+                if topElOffset >= offsetTopMin
+                    if offsetMin == null || offsetMin > Math.abs(topElOffset)
+                        selectMenuEl = menuEl
+
+        unless selectMenuEl?.classList.contains(classSelected)
+            selectMenuEl?.classList.add(classSelected)
+
+    parallax: ->
+        els = [
+            document.getElementById('header')
+            document.getElementById('section-bg-inline')
+        ]
+
+        offsetY = scroll;
+        offsetY = 0 if offsetY < 0;
+
+        for el in els
+            continue unless el
+
+            isElVisible = scroll > -1
+            isElVisible = isElVisible && scroll >= (el.offsetTop - el.offsetHeight)
+            isElVisible = isElVisible && scroll <= (el.offsetTop + el.offsetHeight)
+
+            if isElVisible
+                bgOffset = offsetY - el.offsetTop
+                el.style.backgroundPositionY = "#{bgOffset}px"
+
+#
+app =
+    init: ->
+        app.initScrollHook()
+        app.initGoToTop()
+
+    initScrollHook: ->
+        window.onscroll = -> helpers.scrollHook()
+        window.onscroll()
+
+    initGoToTop: ->
+        for el in document.getElementsByClassName('go-to-top')
+            do (el) ->
+                el.onclick = (e) ->
+                    e.preventDefault();
+
+                    elementID = el.getAttribute('data-top');
+
+                    if elementID
+                        topValue = document.getElementById(elementID).offsetTop
+                    else
+                        topValue = 0
+
+                    if el.hasAttribute('data-top-offset')
+                        topOffset = parseInt el.getAttribute('data-top-offset')
+                        topValue += topOffset
+
+                    helpers.scrollHookMenuReset()
+                    el.classList.add('selected')
+
+                    helpers.scrollTo topValue, 1000, ->
+                        helpers.scrollHookMenuReset()
+                        el.classList.add('selected')
+#
+app.init()
--- shared/site/assets/app.js
+++ shared/site/assets/app.js
@@ -1,62 +1,175 @@
-var el, scrollTo, _fn, _i, _len, _ref;
+var app, helpers;
 
-scrollTo = function(val, duration) {
-  var e, easeOutCuaic, from, scrollToX, t, to;
-  if (duration < 0) {
-    return;
-  }
-  e = document.documentElement;
-  if (e.scrollTop === 0) {
-    t = e.scrollTop;
-    ++e.scrollTop;
-    if (t + 1 !== e.scrollTop--) {
-      e = document.body;
-    }
-  }
-  from = e.scrollTop;
-  to = val;
-  if (typeof from === "object") {
-    from = from.offsetTop;
-  }
-  if (typeof to === "object") {
-    to = to.offsetTop;
-  }
-  easeOutCuaic = function(t) {
-    t--;
-    return t * t * t + 1;
-  };
-  scrollToX = function(element, x1, x2, t, v, step, operacion) {
-    if (t < 0 || t > 1 || v <= 0) {
+helpers = {
+  scrollTo: function(val, duration, done) {
+    var e, easeOutCuaic, from, scrollToX, t, to;
+    if (duration < 0) {
       return;
     }
-    element.scrollTop = x1 - (x1 - x2) * operacion(t);
-    t += v * step;
-    return setTimeout(function() {
-      return scrollToX(element, x1, x2, t, v, step, operacion);
-    }, step);
-  };
-  return scrollToX(e, from, to, 0, 1 / duration, 20, easeOutCuaic);
+    if (window.scrollToWorking === true) {
+      return;
+    }
+    e = document.documentElement;
+    if (e.scrollTop === 0) {
+      t = e.scrollTop;
+      ++e.scrollTop;
+      if (t + 1 !== e.scrollTop--) {
+        e = document.body;
+      }
+    }
+    from = e.scrollTop;
+    to = val;
+    if (typeof from === "object") {
+      from = from.offsetTop;
+    }
+    if (typeof to === "object") {
+      to = to.offsetTop;
+    }
+    easeOutCuaic = function(t) {
+      t--;
+      return t * t * t + 1;
+    };
+    scrollToX = function(element, x1, x2, t, v, step, operacion, done1) {
+      if (t < 0 || t > 1 || v <= 0) {
+        return done1();
+      }
+      element.scrollTop = x1 - (x1 - x2) * operacion(t);
+      t += v * step;
+      return setTimeout(function() {
+        return scrollToX(element, x1, x2, t, v, step, operacion, done1);
+      }, step);
+    };
+    window.scrollToWorking = true;
+    return scrollToX(e, from, to, 0, 1 / duration, 20, easeOutCuaic, function() {
+      window.scrollToWorking = false;
+      return done();
+    });
+  },
+  scrollHook: function() {
+    helpers.scrollHookHeader();
+    return helpers.scrollHookMenu();
+  },
+  scrollHookHeader: function() {
+    var className, classNameAnimate, headerMenuEl, offsetTop, scroll;
+    scroll = document.body.scrollTop;
+    headerMenuEl = document.getElementById('header-menu');
+    offsetTop = document.getElementById('learn-more-button').offsetTop;
+    className = 'open';
+    classNameAnimate = headerMenuEl.getAttribute('data-animate-effect');
+    if (scroll >= offsetTop) {
+      headerMenuEl.classList.add(className);
+      return headerMenuEl.classList.add(classNameAnimate);
+    } else {
+      headerMenuEl.classList.remove(className);
+      return headerMenuEl.classList.remove(classNameAnimate);
+    }
+  },
+  scrollHookMenuReset: function() {
+    var classSelected, selectedEl, selectedEls, _i, _len, _results;
+    classSelected = 'selected';
+    selectedEls = document.getElementById('menu').getElementsByClassName(classSelected);
+    _results = [];
+    for (_i = 0, _len = selectedEls.length; _i < _len; _i++) {
+      selectedEl = selectedEls[_i];
+      _results.push(selectedEl.classList.remove(classSelected));
+    }
+    return _results;
+  },
+  scrollHookMenu: function() {
+    var classSelected, dataTopEl, menuEl, menuEls, offsetMin, offsetTopMin, scroll, selectMenuEl, topEl, topElOffset, _i, _len;
+    scroll = document.body.scrollTop;
+    helpers.scrollHookMenuReset();
+    classSelected = 'selected';
+    menuEls = document.getElementById('menu').getElementsByTagName('a');
+    offsetTopMin = -100;
+    offsetMin = null;
+    selectMenuEl = null;
+    for (_i = 0, _len = menuEls.length; _i < _len; _i++) {
+      menuEl = menuEls[_i];
+      dataTopEl = menuEl.getAttribute('data-top');
+      if (dataTopEl) {
+        topEl = document.getElementById(dataTopEl);
+        topElOffset = scroll - (topEl.offsetTop + offsetTopMin);
+        if (topElOffset >= offsetTopMin) {
+          if (offsetMin === null || offsetMin > Math.abs(topElOffset)) {
+            selectMenuEl = menuEl;
+          }
+        }
+      }
+    }
+    if (!(selectMenuEl != null ? selectMenuEl.classList.contains(classSelected) : void 0)) {
+      return selectMenuEl != null ? selectMenuEl.classList.add(classSelected) : void 0;
+    }
+  },
+  parallax: function() {
+    var bgOffset, el, els, isElVisible, offsetY, _i, _len, _results;
+    els = [document.getElementById('header'), document.getElementById('section-bg-inline')];
+    offsetY = scroll;
+    if (offsetY < 0) {
+      offsetY = 0;
+    }
+    _results = [];
+    for (_i = 0, _len = els.length; _i < _len; _i++) {
+      el = els[_i];
+      if (!el) {
+        continue;
+      }
+      isElVisible = scroll > -1;
+      isElVisible = isElVisible && scroll >= (el.offsetTop - el.offsetHeight);
+      isElVisible = isElVisible && scroll <= (el.offsetTop + el.offsetHeight);
+      if (isElVisible) {
+        bgOffset = offsetY - el.offsetTop;
+        _results.push(el.style.backgroundPositionY = bgOffset + "px");
+      } else {
+        _results.push(void 0);
+      }
+    }
+    return _results;
+  }
 };
 
-_ref = document.getElementsByClassName('go-to-top');
-_fn = function(el) {
-  return el.onclick = function(e) {
-    var elementID, topOffset, topValue;
-    e.preventDefault();
-    elementID = el.getAttribute('data-top');
-    if (elementID) {
-      topValue = document.getElementById(elementID).offsetTop;
-    } else {
-      topValue = 0;
+app = {
+  init: function() {
+    app.initScrollHook();
+    return app.initGoToTop();
+  },
+  initScrollHook: function() {
+    window.onscroll = function() {
+      return helpers.scrollHook();
+    };
+    return window.onscroll();
+  },
+  initGoToTop: function() {
+    var el, _i, _len, _ref, _results;
+    _ref = document.getElementsByClassName('go-to-top');
+    _results = [];
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      el = _ref[_i];
+      _results.push((function(el) {
+        return el.onclick = function(e) {
+          var elementID, topOffset, topValue;
+          e.preventDefault();
+          elementID = el.getAttribute('data-top');
+          if (elementID) {
+            topValue = document.getElementById(elementID).offsetTop;
+          } else {
+            topValue = 0;
+          }
+          if (el.hasAttribute('data-top-offset')) {
+            topOffset = parseInt(el.getAttribute('data-top-offset'));
+            topValue += topOffset;
+          }
+          helpers.scrollHookMenuReset();
+          el.classList.add('selected');
+          return helpers.scrollTo(topValue, 1000, function() {
+            helpers.scrollHookMenuReset();
+            return el.classList.add('selected');
+          });
+        };
+      })(el));
     }
-    if (el.hasAttribute('data-top-offset')) {
-      topOffset = parseInt(el.getAttribute('data-top-offset'));
-      topValue += topOffset;
-    }
-    return scrollTo(topValue, 1000);
-  };
+    return _results;
+  }
 };
-for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-  el = _ref[_i];
-  _fn(el);
-}
+
+app.init();
