06ffa609295081a6d64d89501503fd7ba7340d3e
itspoma <itspoma@gmail.com>
2015-02-08 01:29:15 +0200

    added course route, refactoring

--- shared/site/src/app.php
+++ shared/site/src/app.php
@@ -1,17 +1,25 @@
 <?php
 use Igorw\Silex\ConfigServiceProvider;
-use Silex\Provider\HttpCacheServiceProvider;
-use Silex\Provider\TwigServiceProvider;
 
 $configFilePattern = sprintf("%s/config/%s.yml", SRC_DIR, '%s');
 $app->register(new ConfigServiceProvider(sprintf($configFilePattern, 'default')));
 $app->register(new ConfigServiceProvider(sprintf($configFilePattern, $env)));
 
-$app->register(new HttpCacheServiceProvider(), array(
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
+
+$app->register(new Silex\Provider\HttpCacheServiceProvider(), array(
     'http_cache.cache_dir' => ROOT_DIR . '/cache'
 ));
 
-$app->register(new TwigServiceProvider(), array(
+$app->register(new Moust\Silex\Provider\CacheServiceProvider(), array(
+    'cache.options' => array(
+        'driver' => 'file',
+        'cache_dir' => ROOT_DIR . '/cache/app',
+    ),
+
+));
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.options'        => array(
         'cache'            => isset($app['twig.options.cache']) ? $app['twig.options.cache'] : false,
         'strict_variables' => true
--- shared/site/src/controllers.php
+++ shared/site/src/controllers.php
@@ -3,8 +3,52 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 $app->match('/', function () use ($app) {
-    return $app['twig']->render('landing/index.html.twig');
-})->bind('landing');
+    return $app['twig']->render('landing/index.html.twig', array(
+        'courses' => $app['cache']->fetch('db.google.courses.data'),
+        'partners' => $app['cache']->fetch('db.google.partners.data'),
+    ));
+})
+->bind('landing');
+
+$app->match('/course/{id}', function () use ($app) {
+    var_dump('course');
+})
+->bind('course');
+
+// move to helpers
+$app['google-api-fetch'] = $app->protect(function ($key) use ($app) {
+    $url = sprintf(
+        'https://docs.google.com/spreadsheets/d/%s/export?gid=%s&format=%s',
+        $app['db.google.'.$key.'.key'],
+        $app['db.google.'.$key.'.gid'],
+        'tsv'
+    );
+
+    $csv = trim(file_get_contents($url));
+    $csv = split("\n", $csv);
+
+    $columns = split("\t", $csv[0]);
+    $records = array();
+
+    for ($i = 1; $i < count($csv); $i++) {
+        $row = split("\t", $csv[$i]);
+        $records[] = array_combine($columns, $row);
+    }
+
+    $app['cache']->store('db.google.'.$key.'.data', $records);
+
+    return $records;
+});
+
+$app->match('/admin/update', function () use ($app) {
+    $records = $app['google-api-fetch']('courses');
+    $partners = $app['google-api-fetch']('partners');
+    //
+
+    var_dump($records);die;
+
+    return 'test';
+});
 
 $app->error(function (\Exception $e, $code) use ($app) {
     if ($app['debug']) {
