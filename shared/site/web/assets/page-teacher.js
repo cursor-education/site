500cfcc12f023d5ef010e8183765e91596f359ac
itspoma <itspoma@gmail.com>
2015-02-18 22:35:07 +0200

    added generated assets

--- shared/site/web/assets/page-teacher.js
+++ shared/site/web/assets/page-teacher.js
@@ -1,2 +1,2 @@
-/* 2015-02-17 21:02:39 */
+/* 2015-02-18 20:02:47 */
 var app;app={init:function(){return app.initScrollHook(),helpers.initTooltip()},initScrollHook:function(){return window.onscroll=function(){return helpers.scrollHook()},window.onscroll()}},app.init();
