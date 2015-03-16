f069fd796a78bf59eddf96654222c65a9925a8bd
itspoma <itspoma@gmail.com>
2015-03-16 22:52:23 +0200

    added generated static

--- shared/site/web/assets/page-teacher.js
+++ shared/site/web/assets/page-teacher.js
@@ -1,2 +1,2 @@
-/* 2015-03-16 17:03:16 */
+/* 2015-03-16 20:03:10 */
 var app;app={init:function(){return app.initScrollHook(),helpers.initTooltip()},initScrollHook:function(){return window.onscroll=function(){return helpers.scrollHook()},window.onscroll()}},app.init();
