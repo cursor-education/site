0b019699e1b09e05c23c9df1b7110166be27e84e
itspoma <itspoma@gmail.com>
2015-03-16 19:34:48 +0200

    updated generated static

--- shared/site/web/assets/page-teacher.js
+++ shared/site/web/assets/page-teacher.js
@@ -1,2 +1,2 @@
-/* 2015-03-16 00:03:35 */
+/* 2015-03-16 17:03:16 */
 var app;app={init:function(){return app.initScrollHook(),helpers.initTooltip()},initScrollHook:function(){return window.onscroll=function(){return helpers.scrollHook()},window.onscroll()}},app.init();
