4d44f414d4773f030ce45a4f11bc5ad3ad4a7296
itspoma <itspoma@gmail.com>
2015-03-16 02:51:56 +0200

    updated generated assets

--- shared/site/web/assets/page-course.js
+++ shared/site/web/assets/page-course.js
@@ -1,2 +1,2 @@
-/* 2015-02-18 22:02:46 */
+/* 2015-03-16 00:03:35 */
 var app;app={init:function(){return app.initScrollHook(),helpers.initTooltip(),helpers.initSubmitForm()},initScrollHook:function(){return window.onscroll=function(){return helpers.scrollHook()},window.onscroll()}},app.init();
