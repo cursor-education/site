c8bb393d4a8492361cffec8ce0187195395e11d9
itspoma <itspoma@gmail.com>
2015-03-17 17:36:43 +0200

    updated generated static

--- shared/site/web/assets/page-landing.js
+++ shared/site/web/assets/page-landing.js
@@ -1,2 +1,2 @@
-/* 2015-03-16 22:03:38 */
+/* 2015-03-17 15:03:14 */
 var app;app={init:function(){return app.initScrollHook(),helpers.initScrollToTopEls(),helpers.initSubmitForm()},initScrollHook:function(){return window.onscroll=function(){return helpers.scrollHook()},window.onscroll()}},app.init();
--- shared/site/web/assets/page-teacher.js
+++ shared/site/web/assets/page-teacher.js
@@ -1,2 +1,2 @@
-/* 2015-03-16 22:03:38 */
+/* 2015-03-17 15:03:14 */
 var app;app={init:function(){return app.initScrollHook(),helpers.initTooltip()},initScrollHook:function(){return window.onscroll=function(){return helpers.scrollHook()},window.onscroll()}},app.init();
