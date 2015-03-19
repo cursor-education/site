8e910d666c432fc4492dcfe4be45dfeb569345f5
itspoma <itspoma@gmail.com>
2015-03-19 17:33:21 +0200

    updated generated static

--- shared/site/web/assets/page-course.js
+++ shared/site/web/assets/page-course.js
@@ -1,2 +1,2 @@
-/* 2015-03-17 15:03:59 */
+/* 2015-03-19 15:03:48 */
 var app;app={init:function(){return app.initScrollHook(),helpers.initTooltip(),helpers.initSubmitForm()},initScrollHook:function(){return window.onscroll=function(){return helpers.scrollHook()},window.onscroll()}},app.init();
--- shared/site/web/assets/page-landing.js
+++ shared/site/web/assets/page-landing.js
@@ -1,2 +1,2 @@
-/* 2015-03-17 15:03:59 */
-var app;app={init:function(){return app.initScrollHook(),helpers.initScrollToTopEls(),helpers.initSubmitForm()},initScrollHook:function(){return window.onscroll=function(){return helpers.scrollHook()},window.onscroll()}},app.init();
+/* 2015-03-19 15:03:48 */
+var app;app={init:function(){return app.initScrollHook(),helpers.initScrollToTopEls(),helpers.initSubmitForm(),helpers.initAbTesting()},initScrollHook:function(){return window.onscroll=function(){return helpers.scrollHook()},window.onscroll()}},app.init();
