cf9c6af506bb81493ad3e4cb4b3a09cbe52c9b2b
itspoma <itspoma@gmail.com>
2015-03-27 18:39:51 +0200

    updated static

--- shared/site/web/assets/page-teacher.js
+++ shared/site/web/assets/page-teacher.js
@@ -1,2 +1,2 @@
-/* 2015-03-27 15:03:50 */
+/* 2015-03-27 16:03:29 */
 var app;app={init:function(){return app.initScrollHook(),helpers.initTooltip()},initScrollHook:function(){return window.onscroll=function(){return helpers.scrollHook()},window.onscroll()}},app.init();
