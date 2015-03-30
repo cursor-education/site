d0118ecc12157dae0c0dc02a8f1b04a64165f79a
itspoma <itspoma@gmail.com>
2015-03-30 23:41:39 +0300

    updated static

--- shared/site/web/assets/page-course.js
+++ shared/site/web/assets/page-course.js
@@ -1,2 +1,2 @@
-/* 2015-03-28 20:03:35 */
+/* 2015-03-30 20:03:30 */
 var app;app={init:function(){return app.initScrollHook(),helpers.initTooltip(),helpers.initSubmitForm()},initScrollHook:function(){return window.onscroll=function(){return helpers.scrollHook()},window.onscroll()}},app.init();
