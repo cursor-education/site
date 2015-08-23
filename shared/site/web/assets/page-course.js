d4f1ad300ff4d285d394044e973cff39eefa96d6
itspoma <itspoma@gmail.com>
2015-08-24 00:36:22 +0300

    added generated static

--- shared/site/web/assets/page-course.js
+++ shared/site/web/assets/page-course.js
@@ -1,2 +1,2 @@
-/* 2015-08-23 17:08:12 */
+/* 2015-08-23 21:08:10 */
 var app;app={init:function(){return app.initScrollHook(),helpers.initTooltip(),helpers.initSubmitForm(),helpers.initTooltip()},initScrollHook:function(){return window.onscroll=function(){return helpers.scrollHook()},window.onscroll()}},app.init();
