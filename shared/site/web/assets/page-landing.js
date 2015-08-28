4b90ca9171c7ac4891518a5c1dc8a41e4ce210b9
itspoma <itspoma@gmail.com>
2015-08-28 23:42:48 +0300

    updated teaches section for mobile

--- shared/site/web/assets/page-landing.js
+++ shared/site/web/assets/page-landing.js
@@ -1,2 +1,2 @@
-/* 2015-08-23 21:08:10 */
+/* 2015-08-28 20:08:15 */
 var app;app={init:function(){return app.initScrollHook(),helpers.initScrollToTopEls(),helpers.initSubmitForm(),helpers.initAbTesting(),helpers.initTooltip()},initScrollHook:function(){return window.onscroll=function(){return helpers.scrollHook()},window.onscroll()}},app.init();
--- shared/site/web/assets/page-teacher.js
+++ shared/site/web/assets/page-teacher.js
@@ -1,2 +1,2 @@
-/* 2015-08-23 21:08:10 */
+/* 2015-08-28 20:08:15 */
 var app;app={init:function(){return app.initScrollHook(),helpers.initTooltip(),app.initPageBackground()},initScrollHook:function(){return window.onscroll=function(){return helpers.scrollHook()},window.onscroll()},initPageBackground:function(){var a;return a=document.getElementsByTagName("body")[0],teacher.background_image?a.style.backgroundImage="url("+teacher.background_image+")":void 0}},app.init();
