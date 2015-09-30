bec0ddce19f2802b179cc34f1726fe848964175a
itspoma <itspoma@gmail.com>
2015-09-30 18:05:21 +0300

    regenerated static

--- shared/site/web/assets/page-course.js
+++ shared/site/web/assets/page-course.js
@@ -1,2 +1,2 @@
-/* 2015-09-03 20:09:05 */
+/* 2015-09-30 15:09:02 */
 var app;app={init:function(){return app.initScrollHook(),helpers.initTooltip(),helpers.initSubmitForm(),helpers.initTooltip()},initScrollHook:function(){return window.onscroll=function(){return helpers.scrollHook()},window.onscroll()}},app.init();
