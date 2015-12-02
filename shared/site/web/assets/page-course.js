b8244170f1f8ace038d8cda7969376196e72cd34
itspoma <itspoma@gmail.com>
2015-12-02 02:44:01 +0200

    regenerated static

--- shared/site/web/assets/page-course.js
+++ shared/site/web/assets/page-course.js
@@ -1,2 +1,2 @@
-/* 2015-12-02 00:12:07 */
+/* 2015-12-02 00:12:42 */
 var app;app={init:function(){return app.initScrollHook(),helpers.initTooltip(),helpers.initSubmitForm(),helpers.initTooltip()},initScrollHook:function(){return window.onscroll=function(){return helpers.scrollHook()},window.onscroll()}},app.init();
--- shared/site/web/assets/page-promo.js
+++ shared/site/web/assets/page-promo.js
@@ -1,2 +1,2 @@
-/* 2015-12-02 00:12:07 */
+/* 2015-12-02 00:12:42 */
 var app;app={init:function(){return helpers.initTooltip()}},app.init();
