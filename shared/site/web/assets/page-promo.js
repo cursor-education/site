1e2d5451b1ed14c22df5ad38ecb5f5453714b67a
itspoma <itspoma@gmail.com>
2015-12-16 01:04:18 +0200

    regenerated static

--- shared/site/web/assets/page-promo.js
+++ shared/site/web/assets/page-promo.js
@@ -1,2 +1,2 @@
-/* 2015-12-15 22:12:52 */
+/* 2015-12-15 23:12:12 */
 var app;app={init:function(){return helpers.initTooltip()}},app.init();
--- shared/site/web/assets/page-workshop.js
+++ shared/site/web/assets/page-workshop.js
@@ -1,2 +1,2 @@
-/* 2015-12-15 22:12:52 */
-var app;app={init:function(){return app.initScrollHook(),helpers.initTooltip(),helpers.initSubmitForm(),helpers.initTooltip()},initScrollHook:function(){return window.onscroll=function(){return helpers.scrollHook()},window.onscroll()}},app.init();
+/* 2015-12-15 23:12:12 */
+var app;app={init:function(){return helpers.initMap(),app.initScrollHook(),helpers.initTooltip(),helpers.initSubmitForm(),helpers.initTooltip()},initScrollHook:function(){return window.onscroll=function(){return helpers.scrollHook()},window.onscroll()}},app.init();
