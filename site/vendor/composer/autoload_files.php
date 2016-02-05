a5c6915d8be2b0fd89721305c47fce11607405d8
itspoma <itspoma@gmail.com>
2016-02-05 03:07:58 +0200

    updated vendors

--- site/vendor/composer/autoload_files.php
+++ site/vendor/composer/autoload_files.php
@@ -7,4 +7,5 @@ $baseDir = dirname($vendorDir);
 
 return array(
     '3e2471375464aac821502deb0ac64275' => $vendorDir . '/symfony/polyfill-php54/bootstrap.php',
+    '2c102faa651ef8ea5874edb585946bce' => $vendorDir . '/swiftmailer/swiftmailer/lib/swift_required.php',
 );
